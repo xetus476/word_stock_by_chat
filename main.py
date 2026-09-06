1ff59baf3023168555650dcc841d8ca2b396a844
xetus476 <xetus476@gmail.com>
2026-09-07 00:51:24 +0700

    only start

--- main.py
+++
@@ -1,8 +0,0 @@
-a, b = map(int, input().split())
-sp = list(map(int, input().split()))
-new_sp = []
-for i in range(0, a-1):
-    for j in range(i+1,a-1):
-        c=sp[i]*sp[j]
-        new_sp.append(c)
-print(new_sp)
